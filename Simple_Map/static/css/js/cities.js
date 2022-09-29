952091749ef07bc4831d79cd8a0591c8faf6fc93
MichaelOkoro24 <michaelokoro24@gmail.com>
2022-09-29 14:15:40 -0400

    geomap

--- Simple_Map/static/css/js/cities.js
+++ Simple_Map/static/css/js/cities.js
@@ -32,3 +32,14 @@ let cities = [{
   }
   ];
 
+
+
+
+
+  // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  accessToken: API_KEY
+});
